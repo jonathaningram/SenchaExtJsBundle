4fa5b6c5aefe25d29fa21ea1e81cf3b2bb22c48d
Jonathan Ingram <jonathan.b.ingram@gmail.com>
2012-01-05 11:03:42 +1100

    Updated version

--- Version.php
+++ Version.php
@@ -7,21 +7,7 @@ final class Version
 	/**
 	 * ExtJS version identification
 	 */
-	const VERSION = self::FOUR_ZERO_SEVEN;
-	
-	const THREE_THREE_ONE	= '3.3.1';
-    const FOUR_PR_FOUR		= '4.0-pr4';
-	const FOUR_PR_FIVE		= '4.0-pr5';
-	const FOUR_BETA_ONE		= '4.0-beta1';
-	const FOUR_BETA_TWO		= '4.0-beta2';
-	const FOUR_BETA_THREE	= '4.0-beta3';
-	const FOUR_ZERO_ZERO	= '4.0.0';
-	const FOUR_ZERO_ONE		= '4.0.1';
-	const FOUR_ZERO_TWO		= '4.0.2';
-	const FOUR_ZERO_TWO_A	= '4.0.2a';
-	const FOUR_ZERO_FIVE	= '4.0.5';
-	const FOUR_ZERO_SIX		= '4.0.6';
-	const FOUR_ZERO_SEVEN	= '4.0.7';
+	const VERSION = '4.1.0-beta-1';
 	
 	/**
 	 * Constants of version parts
